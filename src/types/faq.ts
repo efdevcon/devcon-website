520496066cbbc32b52af058e2a860bc714e3fc31
wslyvh <wslyvh@users.noreply.github.com>
2020-11-27 15:49:27 +0100

    base cms structure

---
+++ src/types/faq.ts
@@ -0,0 +1,9 @@
+import { Category } from "./category"
+
+export interface FAQ {
+    title: string;
+    body: string;
+    category: Array<Category>
+    order?: number
+    lang: string
+}
---
+++ src/types/tag.ts
@@ -0,0 +1,3 @@
+export interface Tag {
+    title: string;
+}
