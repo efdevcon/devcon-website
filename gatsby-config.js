33dd2e8c6fb3ac72c2921204a37d9427187992ce
lassejaco <lasse.jacobsen@ethereum.org>
2020-12-08 19:21:05 +0100

    Readd remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
         path: `${__dirname}/src/content/dips`,
       },
     },
+    'gatsby-transformer-remark',
     {
       resolve: `gatsby-plugin-intl`,
       options: {
