23370acb37b09ba835f1031fcfd078b2ff7dced6
wslyvh <wslyvh@users.noreply.github.com>
2021-01-28 12:15:35 +0100

    faq page section and filter

--- src/components/faq/FAQ.tsx
+++ src/components/faq/FAQ.tsx
@@ -4,48 +4,46 @@ import { Category } from 'src/types/Category'
 import { FAQ as FaqType } from 'src/types/FAQ'
 import css from './faq.module.scss'
 
-import AskDeva from 'src/assets/images/ask-deva.png'
 import IconMinus from 'src/assets/icons/minus.svg'
-import { useFormField } from 'src/hooks/useFormField'
 
 interface FaqProps {
   data: Array<Category>
+  filter?: string
   customCategoryTitle?: string // ideally used with a single FAQ category
 }
 
-export function FAQ(props: FaqProps) {
-  const searchField = useFormField()
-  
+export function FAQ(props: FaqProps) {  
   const renderTitle = (title: string) => {
     if (props.customCategoryTitle) return props.customCategoryTitle
 
     return title;
   }
 
+  function toggleExpand(category: string) { 
+    console.log("EXPAND", category);
+  }
+
+  function filter(questions: Array<FaqType>): Array<FaqType> { 
+    if (!props.filter) return questions;
+
+    const filter = props.filter.toLowerCase();
+    return questions.filter(i => i.title.toLowerCase().includes(filter) || 
+      i.body.toLowerCase().includes(filter))
+  }
+
   return (
     <div>
-      {/* <div className={css['filter-container']}>
-        <div className={css['search-bar']}>
-          <input type="text" placeholder='Filter by keywords' {...searchField} />
-          <span className={css['subtitle']}>Filter FAQs using relevant key terms.</span>
-        </div>
-        <div>
-          <img src={AskDeva} alt="Ask Deva" />
-        </div>
-      </div> */}
-
       {props.data.map((category: Category) => {
           return (
             <>
               <div id={category.id} className={css['category']}>
                 <h3 key={category.id}>{renderTitle(category.title)}</h3>
-                {/* <span role='button' className={css['collapse']}><IconMinus /></span> */}
+                <span role='button' className={css['collapse']} onClick={() => toggleExpand(category.title)}><IconMinus /></span>
               </div>
 
-              {category.questions.length > 0 &&
-                category.questions.map((question: FaqType) => {
-                  return <Collapse title={question.title} body={question.body} />
-                })}
+              {category.questions.length > 0 && filter(category.questions).map((question: FaqType) => {
+                return <Collapse title={question.title} body={question.body} expanded={true} />
+              })}
             </>
           )
       })}
