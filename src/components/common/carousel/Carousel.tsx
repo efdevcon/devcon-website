b5dae23999d0e76f59eb2ba348cc7a5a3afce475
wslyvh <wslyvh@users.noreply.github.com>
2021-02-10 19:18:58 +0100

    carousel

--- src/components/common/carousel/Carousel.tsx
+++ src/components/common/carousel/Carousel.tsx
@@ -6,6 +6,7 @@ import Image2 from './images/image2.jpg'
 import Image3 from './images/image3.jpg'
 import Image4 from './images/image4.jpg'
 import Image5 from './images/image5.jpg'
+import Image6 from './images/image6.jpg'
 
 export function Carousel() {
 
@@ -13,13 +14,20 @@ export function Carousel() {
     <div className={css['container']}>
       <h3>Bogota Colombia</h3>
 
-      <ul className={css['images']}>
-        {/* <li><img src={Image1} alt="Bogota" /></li>
-        <li><img src={Image2} alt="Bogota" /></li>
-        <li><img src={Image3} alt="Bogota" /></li> */}
-        <li><img src={Image4} alt="Bogota" /></li>
-        <li><img src={Image5} alt="Bogota" /></li>
-      </ul>
+      <div className={css['images']}>
+        <img src={Image1} alt="Bogota" />
+        <img src={Image2} alt="Bogota" />
+        <img src={Image3} alt="Bogota" />
+        <img src={Image4} alt="Bogota" />
+        <img src={Image5} alt="Bogota" />
+        <img src={Image6} alt="Bogota" />
+        <img src={Image1} alt="Bogota" />
+        <img src={Image2} alt="Bogota" />
+        <img src={Image3} alt="Bogota" />
+        <img src={Image4} alt="Bogota" />
+        <img src={Image5} alt="Bogota" />
+        <img src={Image6} alt="Bogota" />
+      </div>
     </div>
   )
 }
