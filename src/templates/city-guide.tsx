de1bbf55b87c5f72925087c9e36c65897ecdbe47
wslyvh <wslyvh@users.noreply.github.com>
2021-02-11 14:21:16 +0100

    city guide fixes

--- src/templates/city-guide.tsx
+++ src/templates/city-guide.tsx
@@ -16,7 +16,8 @@ import { useIntl } from 'gatsby-plugin-intl'
 export default function CityGuideTemplate({ data, location }: any) {
   const intl = useIntl()
   const page = data.markdownRemark
-  const faq = ToFaqData(data)
+  const faqs = ToFaqData(data)
+  const faq = faqs.filter(i => i.id === 'location');
   const todo = {
     title: data.todo.nodes[0].frontmatter.title,
     show_title: data.todo.nodes[0].frontmatter.show_title,
@@ -29,7 +30,7 @@ export default function CityGuideTemplate({ data, location }: any) {
     left: data.why.nodes[0].fields.frontmattermd.left.html,
     right: data.why.nodes[0].fields.frontmattermd.right.html,
   }
-
+  
   return (
     <Content navigationData={data.navigationData} location={location}>
       <SEO title={page.frontmatter.title} description={page.frontmatter.description} lang={page.fields.lang} />
@@ -83,7 +84,7 @@ export default function CityGuideTemplate({ data, location }: any) {
           <TwoColumns id="why-bogota" title={why.title} left={why.left} right={why.right} />
 
           <section id="FAQ" className={css['section']}>
-            <FAQ data={faq.filter(i => i.id === 'location')} customCategoryTitle="Frequently Asked Questions" />
+            <FAQ data={faq} customCategoryTitle="Frequently Asked Questions" />
           </section>
         </div>
       </div>
