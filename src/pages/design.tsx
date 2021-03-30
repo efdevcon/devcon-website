dfe8cc2ec992bcbe6c4acffd692a3e34f38603b3
wslyvh <wslyvh@users.noreply.github.com>
2021-03-30 18:45:08 +0200

    ds

---
+++ src/pages/design.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { DesignSystem } from 'src/components/common/design-system'
+
+export default function Index() {
+  return (
+    <div>
+      <DesignSystem />
+    </div>
+  )
+}
