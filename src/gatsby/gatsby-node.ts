216c9724efbb263f8585a24b7b44625aa86bcdd6
lassejaco <lasse.jacobsen@ethereum.org>
2021-04-08 10:59:02 +0200

    Fix conflicting CSS order warning

--- src/gatsby/gatsby-node.ts
+++ src/gatsby/gatsby-node.ts
@@ -10,3 +10,22 @@ export const createPages: GatsbyNode['createPages'] = createPagesPlugin
 export const onCreatePage: GatsbyNode['onCreatePage'] = onCreatePagePlugin
 export const sourceNodes: GatsbyNode['sourceNodes'] = sourceNodesPlugin
 export const createSchemaCustomization: any = createSchemaCustomizationPlugin // Type issue: https://github.com/gatsbyjs/gatsby/issues/27096
+
+// Ordering doesn't matter since we're using scoped/prefixed classes, which means we can safely ignore order (prevents warnings in build step)
+// https://stackoverflow.com/questions/63124432/how-do-i-configure-mini-css-extract-plugin-in-gatsby
+const ignoreCSSOrder = ({ stage, actions, getConfig }: any) => {
+  if (stage === 'build-javascript') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(
+      (plugin: any) => plugin.constructor.name === 'MiniCssExtractPlugin'
+    )
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true
+    }
+    actions.replaceWebpackConfig(config)
+  }
+}
+
+export const onCreateWebpackConfig = (args: any) => {
+  ignoreCSSOrder(args);
+}
