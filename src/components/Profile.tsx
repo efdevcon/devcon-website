28319ca269d5ad45117c8088487d799b56228244
wslyvh <wslyvh@users.noreply.github.com>
2021-04-22 21:16:37 +0200

    web3 profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Profile() {
+  const [profile, setProfile] = React.useState();
+
+  React.useEffect(() => {
+    async function asyncEffect() { 
+      const response = await fetch('/.netlify/functions/api/users/profile')
+      if(response.status === 200 && response.body) {
+        const body = await response.json()
+        setProfile(body.data)
+      }
+    }
+
+    asyncEffect()
+  }, [])
+  return (
+    <div>
+      <h2>User Profile</h2>
+      {!profile && <span>Not logged in.</span>}
+      {profile && <span>Account: {profile}</span>}
+    </div>
+  )
+}
--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,28 +1,25 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 
 const LazyConnectComponent = React.lazy(() =>
   import("../../components/Connect")
 )
+const LazyProfileComponent = React.lazy(() =>
+  import("../../components/Profile")
+)
 
-export default function Index({ data }: any) {
+export default function Index() {
   const isSSR = typeof window === "undefined"
 
   return (
     <div>
       <h2>Login</h2>
+
       {!isSSR && (
           <React.Suspense fallback={<div />}>
-              <LazyConnectComponent />
+            <LazyConnectComponent />
+            <LazyProfileComponent />
           </React.Suspense>
       )}
     </div>
   )
 }
-
-export const query = graphql`
-  query($language: String!) {
-    ...NavigationData
-    ...NewsData
-  }
-`
