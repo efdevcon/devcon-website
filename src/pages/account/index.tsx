c1cf23c54d9ea86dbc9bd879432198ef8c484a23
wslyvh <wslyvh@users.noreply.github.com>
2021-04-29 14:57:20 +0200

    remove duplicate profile

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -3,9 +3,6 @@ import React from 'react'
 const LazyConnectComponent = React.lazy(() =>
   import("../../components/Connect")
 )
-const LazyProfileComponent = React.lazy(() =>
-  import("../../components/Profile")
-)
 
 export default function Index() {
   const isSSR = typeof window === "undefined"
@@ -17,7 +14,6 @@ export default function Index() {
       {!isSSR && (
           <React.Suspense fallback={<div />}>
             <LazyConnectComponent />
-            <LazyProfileComponent />
           </React.Suspense>
       )}
     </div>
