7fa4b2270e76b09ca31bdfde080df91c5d3e8feb
lassejaco <lasse.jacobsen@ethereum.org>
2021-05-13 09:17:39 +0200

    Add events: EFOdyssey and Greek CC EF

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -7,5 +7,5 @@ export function GetExcerpt(text: string, length: number = 250) {
 }
 
 export function GetDomainName(url: string): string {
-  return url.replace('http://', '').replace('https://', '').replace('www.', '').split(/[/?#]/)[0]
+  return url.replace('http://', '').replace('https://', '').replace('www.', '').split(/[\/?#]/)[0]
 }
