9c74edf2893df0c48cafd3c0d19e6abefe0ba5ab
wslyvh <wslyvh@users.noreply.github.com>
2021-06-29 10:38:49 +0200

    tags submenu

---
+++ src/components/domain/archive/tags.ts
@@ -0,0 +1,35 @@
+export const tags = [
+    {
+        title: 'Society and Systems',
+    },
+    {
+        title: 'Scalability',
+    },
+    {
+        title: 'UX & Design',
+    },
+    {
+        title: 'Security',
+    },
+    {
+        title: 'Cryptography',
+    },
+    {
+        title: 'Community',
+    },
+    {
+        title: 'Economics',
+    },
+    {
+        title: 'Standards',
+    },
+    {
+        title: 'Privacy',
+    },
+    {
+        title: 'Consensus Layer',
+    },
+    {
+        title: 'Application Layer',
+    },
+]
