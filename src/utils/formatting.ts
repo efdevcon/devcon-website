7ad23cec462e0b500a45f9a5530ae04262182e9d
wslyvh <wslyvh@users.noreply.github.com>
2021-08-10 13:48:35 +0200

    ipfs banner

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -9,3 +9,11 @@ export function GetExcerpt(text: string, length: number = 250) {
 export function GetDomainName(url: string): string {
   return url.replace('http://', '').replace('https://', '').replace('www.', '').split(/[/?#]/)[0]
 }
+
+export function TruncateMiddle(text: string, length: number = 5) {
+  if (text.length > (length * 2) + 1) {
+    return `${text.substring(0, length)}...${text.substring(text.length - length, text.length)}`
+  }
+
+  return text
+}
