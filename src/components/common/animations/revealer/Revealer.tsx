ea87bf9fc64b6dd017550092c98159123ef65cbf
lassejaco <lasse.jacobsen@ethereum.org>
2021-09-06 14:00:59 +0200

    Remove things from last commit that got in by mistake

--- src/components/common/animations/revealer/Revealer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Link } from 'src/components/common/link'
-import css from './button.module.scss'
-
-export const HighlightText = (props: any) => {
-  return <p>Hello</p>
-}
