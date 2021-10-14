828707474d98512038c53753c21ca82c6b87d31d
lassejaco <lasse.jacobsen@ethereum.org>
2021-10-14 09:43:38 +0200

    build error fix

--- src/components/domain/app/speakers/SpeakerFilter.tsx
+++ src/components/domain/app/speakers/SpeakerFilter.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import { Filter, useFilter } from 'src/components/common/filter'
-import { Speaker } from 'src/types/Speaker';
+// import React from 'react'
+// import { Filter, useFilter } from 'src/components/common/filter'
+// import { Speaker } from 'src/types/Speaker';
 
-type Props = {
-  speakers: Speaker[]
-}
+// type Props = {
+//   speakers: Speaker[]
+// }
 
-const SpeakerFilter = (props: Props) => {
-  
-  
-  return (
+// const SpeakerFilter = (props: Props) => {
 
-  )
-}
+//   return (
+
+//   )
+// }
