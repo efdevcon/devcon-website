917ed2044a315ab092f2854267e835d13f3a0976
lassejaco <lasse.jacobsen@ethereum.org>
2022-07-18 08:31:15 +0800

    Remove claim copy from raffle page

--- src/pages/raffle-auction.tsx
+++ src/pages/raffle-auction.tsx
@@ -103,17 +103,17 @@ export default pageHOC(function RaffleAuction(props: any) {
               ]}
             />
 
-            <div className={css['claim']}>
+            {/* <div className={css['claim']}>
               <ClaimingCountdown />
-            </div>
+            </div> */}
 
-            <Link to="https://raffle.devcon.org/">
+            {/* <Link to="https://raffle.devcon.org/">
               <Button className="blue lg">
                 {isAfterDate('2022-07-14 14:00')
                   ? intl('tickets_raffle_claim')
                   : intl('tickets_raffle_participate_now')}
               </Button>
-            </Link>
+            </Link> */}
           </div>
         </div>
 
