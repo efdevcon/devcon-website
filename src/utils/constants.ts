88cbc2a0e1a1e543f92b81c4de31ad09b2a02b44
wslyvh <wslyvh@users.noreply.github.com>
2022-09-27 11:52:12 +0200

    efdevcon api

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,7 +6,7 @@ export const LINK_ETHEREUM_FOUNDATION = 'https://ethereum.foundation'
 export const COPYRIGHT_NOTICE = '© 2022 — Ethereum Foundation. All Rights Reserved.'
 export const STREAMING_URL = 'https://live.devcon.org/'
 export const APP_URL = 'https://app.devcon.org/'
-export const API_URL = 'https://api-xe5o.vercel.app/'
+export const API_URL = 'https://efdevcon-api.vercel.app/'
 
 export const BASE_CONTENT_FOLDER = 'src/content'
 
