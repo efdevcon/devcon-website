e9e0858472febc41ae20a23a63f1743f572b6890
lassejaco <lasse.jacobsen@ethereum.org>
2022-10-01 17:01:49 +0200

    More image casing issues

--- src/components/domain/app/venue/Floor.tsx
+++ src/components/domain/app/venue/Floor.tsx
@@ -4,12 +4,12 @@ import css from './floor.module.scss'
 import venueCss from './venue.module.scss'
 import filterCss from 'components/domain/app/app-filter.module.scss'
 
-import FloorBasement from 'assets/images/venue-map/Floor Basement.png'
-import Floor1 from 'assets/images/venue-map/Floor 1.png'
-import Floor2 from 'assets/images/venue-map/Floor 2.png'
-import Floor3 from 'assets/images/venue-map/Floor 3.png'
-import Floor4 from 'assets/images/venue-map/Floor 4.png'
-import Floor5 from 'assets/images/venue-map/Floor 5.png'
+import FloorBasement from 'assets/images/venue-map/FloorBasement.png'
+import Floor1 from 'assets/images/venue-map/Floor1.png'
+import Floor2 from 'assets/images/venue-map/Floor2.png'
+import Floor3 from 'assets/images/venue-map/Floor3.png'
+import Floor4 from 'assets/images/venue-map/Floor4.png'
+import Floor5 from 'assets/images/venue-map/Floor5.png'
 import ArrowLeft from 'assets/icons/chevron_left.svg'
 import ArrowRight from 'assets/icons/chevron_right.svg'
 import { AppNav } from '../navigation'
@@ -24,16 +24,22 @@ interface Props {
   rooms: Room[]
 }
 
-declare const VALID_LAYOUT_VALUES: readonly ["fill", "fixed", "intrinsic", "responsive", "raw", undefined]
+declare const VALID_LAYOUT_VALUES: readonly ['fill', 'fixed', 'intrinsic', 'responsive', 'raw', undefined]
 declare type LayoutValue = typeof VALID_LAYOUT_VALUES[number]
 
 export const getFloorImage = (floor: string, layout: LayoutValue = 'raw', className = '') => {
-  if (floor === 'Floor 1') return <Image src={Floor1} className={className} alt={floor} layout={layout} id="venue-image" priority />
-  if (floor === 'Floor 2') return <Image src={Floor2} className={className} alt={floor} layout={layout} id="venue-image" priority />
-  if (floor === 'Floor 3') return <Image src={Floor3} className={className} alt={floor} layout={layout} id="venue-image" priority />
-  if (floor === 'Floor 4') return <Image src={Floor4} className={className} alt={floor} layout={layout} id="venue-image" priority />
-  if (floor === 'Floor 5') return <Image src={Floor5} className={className} alt={floor} layout={layout} id="venue-image" priority />
-  if (floor === 'S1') return <Image src={FloorBasement} className={className} alt={floor} layout={layout} id="venue-image" priority />
+  if (floor === 'Floor 1')
+    return <Image src={Floor1} className={className} alt={floor} layout={layout} id="venue-image" priority />
+  if (floor === 'Floor 2')
+    return <Image src={Floor2} className={className} alt={floor} layout={layout} id="venue-image" priority />
+  if (floor === 'Floor 3')
+    return <Image src={Floor3} className={className} alt={floor} layout={layout} id="venue-image" priority />
+  if (floor === 'Floor 4')
+    return <Image src={Floor4} className={className} alt={floor} layout={layout} id="venue-image" priority />
+  if (floor === 'Floor 5')
+    return <Image src={Floor5} className={className} alt={floor} layout={layout} id="venue-image" priority />
+  if (floor === 'S1')
+    return <Image src={FloorBasement} className={className} alt={floor} layout={layout} id="venue-image" priority />
 }
 
 export const Floor = (props: Props) => {
@@ -41,8 +47,9 @@ export const Floor = (props: Props) => {
   const pz = usePanzoom()
 
   const filteredRooms = search
-    ? props.rooms.filter(room => room.name.toLowerCase().includes(search) ||
-      room.description.toLowerCase().includes(search))
+    ? props.rooms.filter(
+        room => room.name.toLowerCase().includes(search) || room.description.toLowerCase().includes(search)
+      )
     : props.rooms
 
   return (
@@ -106,16 +113,16 @@ export function FloorNavigator(props: NavigatorProps) {
     }
   }
 
-  return <div className={`${css['navigator']} section`}>
-    <div className={css['shift-end']}>
-      <button className={`${css['content']} app squared sm thin-borders`}
-        onClick={() => navigateFloor('prev')}>
-        <ArrowLeft />
-      </button>
-      <button className={`${css['content']} app squared sm thin-borders`}
-        onClick={() => navigateFloor('next')}>
-        <ArrowRight />
-      </button>
+  return (
+    <div className={`${css['navigator']} section`}>
+      <div className={css['shift-end']}>
+        <button className={`${css['content']} app squared sm thin-borders`} onClick={() => navigateFloor('prev')}>
+          <ArrowLeft />
+        </button>
+        <button className={`${css['content']} app squared sm thin-borders`} onClick={() => navigateFloor('next')}>
+          <ArrowRight />
+        </button>
+      </div>
     </div>
-  </div>
+  )
 }
