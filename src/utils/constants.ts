a92c53ccd5e0b184232aae6c289d9c25f895f36e
lassejaco <lasse.jacobsen@ethereum.org>
2022-11-08 17:08:54 +0100

    Update copyright

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,4 +12,4 @@ export const EMAIL_SPONSORSHIP = 'sponsorships@ethereum.org'
 export const LINK_ETHEREUM_FOUNDATION = 'https://ethereum.foundation'
 export const LINK_SUBMIT_EVENT = 'https://forms.gle/W2yiyNXcEU6fZ4rN8'
 
-export const COPYRIGHT_NOTICE = '© 2021 — Ethereum Foundation. All Rights Reserved.'
+export const COPYRIGHT_NOTICE = '© 2022 — Ethereum Foundation. All Rights Reserved.'
