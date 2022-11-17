04b5c8cf581df8b4bc5cd55eb4fe0f24f59ac11c
lassejaco <lasse.jacobsen@ethereum.org>
2022-11-17 11:17:44 +0100

    Fix SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,17 +10,7 @@ import { SEO } from 'components/domain/seo'
 function App({ Component, pageProps }: any) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/assets/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <SEO />
-      </Head>
+      <SEO />
 
       <NextIntlProvider messages={pageProps.messages}>
         <Component {...pageProps} />
