9cdede303cdbf542825676a9b12cdfcdc0987551
wslyvh <wslyvh@users.noreply.github.com>
2023-03-02 09:19:23 +0100

    sendpulse newsletter

--- src/components/common/newsletter/Newsletter.tsx
+++ src/components/common/newsletter/Newsletter.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import addToMailchimp from 'gatsby-plugin-mailchimp'
 import { useFormField } from 'src/hooks/useFormField'
 import { useIntl } from 'gatsby-plugin-intl'
 import css from './newsletter.module.scss'
 import { Alert } from '../alert'
 import { Button } from 'src/components/common/button'
+import { EMAIL_DEVCON } from 'src/utils/constants'
 
 export interface Result {
   result: 'success' | 'error'
@@ -20,34 +20,12 @@ export const Newsletter = (props: Props) => {
   const emailField = useFormField()
   const [result, setResult] = React.useState<Result | undefined>(undefined)
 
-  const translateMessage = (message: string) => {
-    if (message.includes('Thank you for subscribing')) {
-      return intl.formatMessage({ id: 'newsletter_subscribed' })
-    }
-    if (message.includes('The email you entered is not valid')) {
-      return intl.formatMessage({ id: 'newsletter_notValid' })
-    }
-    if (message.includes('is already subscribed')) {
-      return intl.formatMessage({ id: 'newsletter_alreadySubscribed' })
-    }
-
-    return ''
-  }
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-
-    let result = (await addToMailchimp(emailField.value, {})) as Result
-    result.msg = translateMessage(result.msg)
-    setResult(result)
-  }
-
   function onDismiss() {
     setResult(undefined)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form action="https://login.sendpulse.com/forms/simple/u/eyJ1c2VyX2lkIjo4MjUxNTM4LCJhZGRyZXNzX2Jvb2tfaWQiOjEwNDI3MSwibGFuZyI6ImVuIn0=" method="post">
       <div>
         <p className="semi-bold">{intl.formatMessage({ id: 'newsletter_title' })}</p>
         <div>
@@ -62,10 +40,12 @@ export const Newsletter = (props: Props) => {
                 <input
                   className={css['input']}
                   type="email"
+                  name='email'
                   id={props.id ?? 'newsletter_email'}
                   placeholder={intl.formatMessage({ id: 'newsletter_enter' })}
                   {...emailField}
                 />
+                <input type="hidden" name="sender" value={EMAIL_DEVCON} />
                 <Button className={`white ${css['button']}`} type="submit">
                   {intl.formatMessage({ id: 'newsletter_subscribe' })}
                 </Button>
